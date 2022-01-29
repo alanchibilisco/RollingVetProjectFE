d8c5187d8e42e67de0ed958e1dfddf4f53b71d02
alanChibilisco <alan.chibilisco@gmail.com>
2022-01-29 19:41:45 -0300

    se comenzo con las primeras modificaciones del proyecto base de react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus obcaecati ullam repellendus dolorum unde eum non quaerat iure, nobis vero corporis recusandae eaque, minus fugiat animi veniam explicabo pariatur odit ipsa cumque? Modi, nihil dolor pariatur quis obcaecati mollitia reprehenderit, laboriosam eligendi blanditiis debitis, rerum accusantium numquam. Tenetur esse, inventore voluptatum quas ipsam enim veritatis, recusandae illo dolor doloribus quia non nostrum magnam tempore culpa praesentium. Illo, iste facere. Cupiditate itaque illo consequatur explicabo assumenda dicta maxime, autem quibusdam labore odio accusamus odit deleniti doloremque excepturi provident modi! Alias non esse eligendi consequuntur facere perferendis sint quisquam voluptatem voluptate quas.</p>
     </div>
   );
 }
