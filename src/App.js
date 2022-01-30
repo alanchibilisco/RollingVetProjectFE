4005c61f8cd534e1431853ffd01d682765aa0ac3
alanChibilisco <alan.chibilisco@gmail.com>
2022-01-29 21:26:14 -0300

    se comenzo a realizar el navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/NavBar"
+
+
 
 function App() {
   return (
-    <div className="">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus obcaecati ullam repellendus dolorum unde eum non quaerat iure, nobis vero corporis recusandae eaque, minus fugiat animi veniam explicabo pariatur odit ipsa cumque? Modi, nihil dolor pariatur quis obcaecati mollitia reprehenderit, laboriosam eligendi blanditiis debitis, rerum accusantium numquam. Tenetur esse, inventore voluptatum quas ipsam enim veritatis, recusandae illo dolor doloribus quia non nostrum magnam tempore culpa praesentium. Illo, iste facere. Cupiditate itaque illo consequatur explicabo assumenda dicta maxime, autem quibusdam labore odio accusamus odit deleniti doloremque excepturi provident modi! Alias non esse eligendi consequuntur facere perferendis sint quisquam voluptatem voluptate quas.</p>
+    <div>
+      <NavBar></NavBar> 
+      
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "../App.css";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import logo from './img/LOGOPNG.png';
+
+const NavBar = () => {
+  return (
+    <div className="">
+      <Navbar className="bg-celeste" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home" className="text-white">
+            {" "}
+            <img
+              src={logo}
+              width="150"
+              height="50"
+              className="d-inline-block align-top"
+              alt="Logo Rolling Vet"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className="text-white">
+                INICIO
+              </Nav.Link>
+              <Nav.Link href="#link" className="text-white">
+                QUIENES SOMOS
+              </Nav.Link>
+              <Nav.Link href="#link" className="text-white">
+                NUESTROS SERVICIOS
+              </Nav.Link>
+              <Nav.Link href="#link" className="text-white">
+                LOGIN
+              </Nav.Link>
+              <Nav.Link href="#link" className="text-white">
+                CONTACTANOS
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
