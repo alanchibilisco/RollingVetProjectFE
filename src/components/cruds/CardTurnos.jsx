cee1e10ed229f11ccd64c46b49e81916fe1b7a6b
Alan Chibilisco <alan.chibilisco@gmail.com>
2022-02-11 13:49:18 -0300

    se ajustaron alguno elementos y detalles

--- src/components/cruds/CardTurnos.jsx
+++ src/components/cruds/CardTurnos.jsx
@@ -9,21 +9,14 @@ const CardTurnos = ({turno}) => {
         <Card.Img className="img-fluid" variant="top" src="" />
         <Card.Body>
           <div className="d-flex align-items-center justify-content-center mb-2">
-            <Card.Title className="m-0 text-truncate fw-bold">Mascota: {turno.mascota}</Card.Title>            
+            <Card.Title className="m-0 fw-bold">{turno.mascota}</Card.Title>            
           </div>
           <Card.Text><span className="fw-bold">Veterinario:</span> {turno.veterinario}</Card.Text>
           <Card.Text><span className="fw-bold">Detalle Cita:</span> {turno.detalleCita}</Card.Text>
           <div className="d-flex align-items-center justify-content-between">
             <span className="badge bg-dark fs-6">{turno.fecha}</span>
-            <span className="badge bg-dark fs-6">{turno.hora}</span>
-            
-          </div>
-          {/* <Link
-              to="*"
-              className=" btn btn-dark text-white text-decoration-none my-2"
-            >
-              Borrar
-            </Link> */}
+            <span className="badge bg-dark fs-6">{turno.hora}</span>            
+          </div>         
         </Card.Body>
       </Card>
     </div>
