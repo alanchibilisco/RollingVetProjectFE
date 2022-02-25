1a655b5667aae24c3a8ba0e18eb97cc64beca2c0
Alan Chibilisco <alan.chibilisco@gmail.com>
2022-02-25 11:42:24 -0300

    se completo maquetado y funcionamiento comentarios

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -21,7 +21,7 @@ const Index = ({weather}) => {
       :
       <></>
     }         
-      <Carrousel />      
+       <Carrousel />      
       <Cardservicios />
       <Nuestrosaliados/>
       <Cardsplanes />
