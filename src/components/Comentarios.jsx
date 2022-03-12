303b58fc98fe9fa4ba7381a395f2cf56dad12de1
alanChibilisco <alan.chibilisco@gmail.com>
2022-03-12 09:33:14 -0300

    se agrego validacion de los campos en comentarios

--- src/components/Comentarios.jsx
+++ src/components/Comentarios.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Card, Col, Container, Form, Row } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
 import Swal from "sweetalert2";
 import CardComentarios from "./CardComentarios";
 import { validateTextoEsp, validateTexto } from "./Validaciones";
 
-const Comentarios = ({session}) => {
-  
+const Comentarios = ({ session }) => {
   const URLComentarios = process.env.REACT_APP_API_COMENTARIOS;
   const [comentarios, setComentarios] = useState([]);
   const [user, setUser] = useState("");
   const [message, setMessage] = useState("");
+  const [validated, setValidated] = useState(false);
 
   const getApiComentarios = async () => {
     try {
@@ -21,77 +21,91 @@ const Comentarios = ({session}) => {
     }
   };
 
+  const gralValidate = () => {
+    if (validateTexto(user) && validateTextoEsp(message)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   useEffect(() => {
     getApiComentarios();
   }, []);
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("desde submit comentarios");
-    if (!validateTexto(user) || !validateTextoEsp(message)) {
-      Swal.fire("Ops!", "Algunos de los campos es incorrectos", "Error");
-      return;
-    }
-
-    const newCom = {
-      user,
-      message,
-      "date": new Date().toString()
-    };
-    Swal.fire({
-      title: "Estas Seguro?",
-      text: "No puedes revertir esto",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Guardar",
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        try {
-          const res = await fetch(URLComentarios, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newCom),
-          });
-          if (res.status === 201) {
-            Swal.fire("El comentario se agrego correctamente", "", "success");
-            getApiComentarios();
-            setUser('');
-            setMessage('');
+    const form = e.currentTarget;
+    if (form.checkValidity() !== false && gralValidate()) {
+      const newCom = {
+        user,
+        message,
+        date: new Date().toString(),
+      };
+      Swal.fire({
+        title: "Estas Seguro?",
+        text: "No puedes revertir esto",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Guardar",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          try {
+            const res = await fetch(URLComentarios, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(newCom),
+            });
+            if (res.status === 201) {
+              Swal.fire("El comentario se agrego correctamente", "", "success");
+              getApiComentarios();
+              setUser("");
+              setMessage("");
+            }
+          } catch (error) {
+            console.log(error);
           }
-        } catch (error) {
-          console.log(error);
         }
-      }
-    });
+      });
+    } else {
+      e.stopPropagation();
+      Swal.fire("Ops!", "Algunos de los campos es incorrectos", "error");
+    }
+    setValidated(true);
   };
-
   return (
     <div>
       <Container className="my-3">
         <h2 className="text-center font-celeste-crud">COMENTARIOS</h2>
         <hr />
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicNombre">
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="validationUser">
             <Form.Label className="font-celeste-crud">
               Ingrese su Nombre y Apellido
             </Form.Label>
             <Form.Control
-            value={user}
+              required
+              value={user}
               type="text"
               placeholder="Usuario Rolling"
               minLength={4}
-              maxLength={100}
+              maxLength={60}
               onChange={({ target }) => setUser(target.value.trimStart())}
             ></Form.Control>
+            <Form.Control.Feedback type="invalid">
+              Ingrese su Nombre y Apellido (min. 4 caracteres, max. 60
+              caracteres, SOLO LETRAS)
+            </Form.Control.Feedback>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicDetalle">
+          <Form.Group className="mb-3" controlId="validationMessage">
             <Form.Label className="font-celeste-crud">
               Ingrese su comentario*
             </Form.Label>
             <Form.Control
-            value={message}
+              required
+              value={message}
               as="textarea"
               type="text"
               placeholder="Ej: Hola"
@@ -102,9 +116,14 @@ const Comentarios = ({session}) => {
                 setMessage(target.value.trimStart());
               }}
             ></Form.Control>
+            <Form.Control.Feedback type="invalid">
+              Por favor ingrese un comentario (min. 4 caracteres, max. 500
+              caracteres)
+            </Form.Control.Feedback>
           </Form.Group>
           <div className="text-end">
             <button className="btn-celeste-crud">GUARDAR</button>
+            {/* <Button type="submit" variant="danger">Guardar</Button> */}
           </div>
         </Form>
         <hr />
@@ -112,20 +131,27 @@ const Comentarios = ({session}) => {
 
       <div className="">
         {comentarios.length > 0 ? (
-
-          <Container className="border border-3 p-3">           
-            {comentarios.map((com)=>(             
-            <CardComentarios key={com._id} com={com} URLComentarios={URLComentarios} getApiComentarios={getApiComentarios} session={session}></CardComentarios>
+          <Container className="border border-3 p-3">
+            {comentarios.map((com) => (
+              <CardComentarios
+                key={com._id}
+                com={com}
+                URLComentarios={URLComentarios}
+                getApiComentarios={getApiComentarios}
+                session={session}
+              ></CardComentarios>
             ))}
           </Container>
-                    ) : (
+        ) : (
           <>
             <div>
-              <h2 className="font-celeste-crud text-center">¡NO HAY COMENTARIOS!</h2>
+              <h2 className="font-celeste-crud text-center">
+                ¡NO HAY COMENTARIOS!
+              </h2>
             </div>
           </>
         )}
-      </div>      
+      </div>
     </div>
   );
 };
